a3987f4c502444b8d8f64263dd1c56863ce76e1b
Jozef Cipa <cipa.jozef@gmail.com>
2025-05-21 23:16:31 +0200

    Initial Commit

---
+++ MouseActivity/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MouseActivity
+//
+//  Created by Jozef Cipa on 21.05.2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
